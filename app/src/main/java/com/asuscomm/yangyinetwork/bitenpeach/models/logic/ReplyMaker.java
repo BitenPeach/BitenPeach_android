cb92116eb02b033081f712d9433c7a08151756ad
pjo901018 <pjo901018@korea.ac.kr>
2017-05-30 02:13:03 +0900

    ReplyMaker Stub제작

--- app/src/main/java/com/asuscomm/yangyinetwork/bitenpeach/models/logic/ReplyMaker.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/bitenpeach/models/logic/ReplyMaker.java
@@ -12,12 +12,13 @@ public class ReplyMaker {
         Reply reply = new Reply();
         String body = "";
 
-        body += "주문서 내용입니다.\n";
+        body += "주문서 내용입니다.\n\n";
 
+        body += orderSheet2string(orderSheet);
 
 
         if (OrderSheetValidater.orderSheetFilled(orderSheet)) {
-            body += "이상 내용으로 주문이 완료되었습니다. ";
+            body += "\n이상 내용으로 주문이 완료되었습니다. ";
             body += "주문한 내용과 다른사항이 있으시면 말씀해주세요.";
         }
 
@@ -25,7 +26,21 @@ public class ReplyMaker {
         return reply;
     }
 
-    public static String orderSheet2string() {
-        return "보내는이 : JYP\n";
+    public static String orderSheet2string(OrderSheet orderSheet) {
+        String content = "";
+
+        content += "보내는이 이름 : JYP\n";
+        content += "보내는이 번호 : 01067213773\n";
+
+        content += "받는이 이름 : 최수연\n";
+        content += "받는이 전화번호 : 010-4725-3470\n";
+        content += "받는이 주소 : 서울 강남구 압구정로 113 미성아파트 29동 311호\n";
+
+        content += "복숭아 종류 : 백도 12개짜리\n";
+        content += "복숭아 크기 : 9호\n";
+        content += "복숭아 박스수 : 1 상자\n";
+        content += "가격 : 20000\n";
+
+        return content;
     }
 }
