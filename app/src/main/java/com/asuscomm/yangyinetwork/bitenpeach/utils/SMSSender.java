a577c165239a1178ef1eaa5dd2471fc62e4e48d5
pjo901018 <pjo901018@korea.ac.kr>
2017-05-30 02:12:41 +0900

    SMS 70자 제한 조각내서 해결

--- app/src/main/java/com/asuscomm/yangyinetwork/bitenpeach/utils/SMSSender.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/bitenpeach/utils/SMSSender.java
@@ -12,12 +12,17 @@ import android.util.Log;
 
 import com.asuscomm.yangyinetwork.bitenpeach.models.domain.Reply;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jaeyoung on 2017. 5. 30..
  */
 
 public class SMSSender {
     private static final String TAG = "JYP/SMSSender";
+    private static final int SMS_LIMIT = 70;
+
     public static void sendReply(Reply reply, Activity activity) {
         Log.d(TAG, "sendReply: reply="+reply.toString());
 
@@ -43,6 +48,31 @@ public class SMSSender {
         AppController.getInstance().chkPermission(Manifest.permission.SEND_SMS, activity);
 
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(reply.getPhoneNumber(), null, reply.getBody(), sentIntent, deliveredIntent);
+
+        List<String> textsForSMS = cutTextForSMS(reply.getBody());
+
+        for (String text:
+             textsForSMS) {
+            smsManager.sendTextMessage(reply.getPhoneNumber(), null, text, sentIntent, deliveredIntent);
+            Log.d(TAG, "sendReply: text="+text);
+        }
+    }
+
+    public static List<String> cutTextForSMS(String text) {
+        List<String> texts = new ArrayList<>();
+        int cursor = 0;
+
+        while(text.length() > SMS_LIMIT+cursor) {
+            String sub = text.substring(cursor, cursor+SMS_LIMIT);
+            Log.d(TAG, "cutTextForSMS: substring="+sub);
+            texts.add(sub);
+            cursor += SMS_LIMIT;
+        }
+
+        String last = text.substring(cursor, text.length());
+        Log.d(TAG, "cutTextForSMS: laststring="+last);
+        texts.add(last);
+
+        return texts;
     }
 }
